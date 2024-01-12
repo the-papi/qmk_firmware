1e31939f4a97e55b3a214d8786720bca9c7b291d
Patrik Janoušek <pj@patrikjanousek.cz>
2024-01-12 21:32:40 +0100

    updated keymap and rgb settings

--- keyboards/monsgeek/m1/keymaps/via/keymap.c
+++ keyboards/monsgeek/m1/keymaps/via/keymap.c
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_B] = LAYOUT_all( /* Base */
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,   KC_DEL,           KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_EQL,   KC_BSPC,          KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_END,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGUP,
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(WIN_FN),KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_W] = LAYOUT_all( /* Base */
@@ -48,12 +48,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                   _______,                            _______,MO(WIN_FN),_______,          KC_A,    KC_S,    KC_D),
 
     [WIN_FN] = LAYOUT_all( /* WASD/↑←↓→ */
-        _______, KC_MYCM, KC_MAIL, KC_WSCH, KC_WHOM, KC_MSEL, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, _______,           RGB_MOD,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           RGB_MOD,
+        _______, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______,           _______,
         _______, _______,TG(WIN_W),_______, _______, _______, _______, _______, KC_INS,  DF(MAC_B),KC_PSCR,_______, _______, RGB_MOD,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RGB_MOD, RGB_HUI,           _______,
-        _______, _______, _______, _______, KC_CALC, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,          _______,  RGB_VAI, _______,
-        _______, GU_TOGG, _______,                   _______,                            _______, _______, _______,          RGB_SAD,  RGB_VAD, RGB_SAI),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RGB_MOD, RGB_TOG,           _______,
+        _______, _______, _______, _______, KC_CALC, _______, _______, _______, KC_MUTE, KC_MPRV, KC_MNXT, _______,          _______,  RGB_VAI, _______,
+        _______, GU_TOGG, _______,                   KC_MPLY,                            _______, _______, _______,          RGB_HUD,  RGB_VAD, RGB_HUI),
 
     [MAC_B] = LAYOUT_all( /* Base */
         KC_ESC,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_F5,   KC_F6,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,KC_VOLU,  KC_DEL,           KC_MUTE,
@@ -93,3 +93,34 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static bool win_fn_layer_active = false;
+
+    // Check if WIN_FN layer is activated or deactivated
+    if (layer_state_is(WIN_FN)) {
+        win_fn_layer_active = true;
+    } else {
+        win_fn_layer_active = false;
+    }
+
+    // Check if WIN_FN is active and KC_1 is pressed
+    if (win_fn_layer_active) {
+        switch (keycode) {
+            case KC_1:
+                // Change RGB color to orange
+                rgblight_sethsv(14, 255, 100);  // Set HSV values for orange color
+                return false;  // Skip further processing of this keypress
+            case KC_2:
+                // Change RGB color to orange
+                rgblight_sethsv(183, 255, 100);  // Set HSV values for orange color
+                return false;  // Skip further processing of this keypress
+            case KC_3:
+                // Change RGB color to orange
+                rgblight_sethsv(0, 0, 100);  // Set HSV values for orange color
+                return false;  // Skip further processing of this keypress
+        }
+    }
+
+    return true; // Process all other keypresses normally
+}
